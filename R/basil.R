2aea508387e816836dc858d62499c76a7fa5db8c
RuilinLi <ruilinli@stanford.edu>
2020-10-06 19:42:47 +0000

    include response is num_events in validation set is at least 30

--- R/basil.R
+++ R/basil.R
@@ -32,10 +32,10 @@ basil_base <- function(genotype.pfile, phe.file, responsid, covs, nlambda, lambd
     for (i in 1:length(status))
     {
         s <- status[i]
-        num_event <- sum(phe %>% filter(split == "train") %>% select(all_of(s)))
-        printf("Code: %s, number of events in training set: %-7d", responsid[i], 
+        num_event <- sum(phe %>% filter(split == "val") %>% select(all_of(s)))
+        printf("Code: %s, number of events in validation set: %-7d", responsid[i], 
             num_event)
-        if (num_event < 100)
+        if (num_event < 200)
         {
             id_to_remove <- c(id_to_remove, responsid[i])
             printf("Too few events, removed!")
